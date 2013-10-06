efecd70acc5cdc3c788b7f38a27e90b9abd77f3d
Jon <jwood803@users.noreply.github.com>
2013-10-06 17:51:39 -0400

    Initial commit

---
+++ Album.js
@@ -0,0 +1,23 @@
+var jamendoApp = angular.module('jamendoApp', []);
+
+jamendoApp.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+jamendoApp.controller('Album', function($scope, $http) {
+  $scope.doRequest = function() {
+    $http.get("http://api.jamendo.com/v3.0/albums/?client_id=b6747d04&format=jsonpretty&artist_name=" + $scope.artist)
+         .success(function(data) {
+            if(data.headers.status == 'success') {
+              $scope.albums = data.results;
+            }
+            else {
+              alert("Error - " + data.headers.error_message);
+            }
+         })
+         .error(function(x, status, error) {
+           alert(error);
+         });
+     };
+});
