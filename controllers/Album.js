286e9650ab01bfcf09fde9d4111e9e5c2a8f4517
Jon <jwood803@users.noreply.github.com>
2013-10-21 21:41:40 -0400

    Moved $http.get to it's own service

--- controllers/Album.js
+++ controllers/Album.js
@@ -1,16 +1,5 @@
 jamendoApp.controller('Album', function($scope, $http) {
   $scope.getAlbums = function() {
-    $http.get("http://api.jamendo.com/v3.0/albums/?client_id=b6747d04&format=jsonpretty&artist_name=" + $scope.artist)
-         .success(function(data) {
-            if(data.headers.status == 'success') {
-              $scope.albums = data.results;
-            }
-            else {
-              alert("Error - " + data.headers.error_message);
-            }
-         })
-         .error(function(x, status, error) {
-           alert(error);
-         });
-     };
+     
+   };
 });
--- services/albumsService.js
+++ services/albumsService.js
@@ -1,5 +1,20 @@
-var albumsService = function($http) {
+var albumsService = function($scope, $http) {
 	var serviceBase = "http://api.jamendo.com/v3.0/",
 		albums = null,
 		albumsFactory = {};
+
+	albumsFactory.getAlbums = function() {
+		return $http.get(serviceBase + "albums/?client_id=b6747d04&format=jsonpretty&artist_name=" + $scope.artist)
+         .success(function(data) {
+            if(data.headers.status == 'success') {
+              $scope.albums = data.results;
+            }
+            else {
+              alert("Error - " + data.headers.error_message);
+            }
+         })
+         .error(function(x, status, error) {
+           alert(error);
+         });
+	};
 }
